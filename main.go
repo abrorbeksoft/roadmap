840feefd660af89a2a22045f6423bc658592056b
abrorbeksoft <abrorbek.ubaydullayev98@gmail.com>
2021-06-23 09:56:06 +0500

    connection to database postgresql id finished

--- main.go
+++ main.go
@@ -1,20 +1,57 @@
 package main
 
 import (
+	"bufio"
+	"database/sql"
 	"fmt"
 	_ "github.com/lib/pq"
+	"os"
 )
 
-const (
-	host= "localhost"
-	port =5432
-	user="user"
-	password="password"
-	dbname="belt_master"
-	)
+type user struct {
+	id int
+	name string
+	surname string
+
+}
 
 func main()  {
 
-	fmt.Println("Hello world")
+	conectStr:="user=root password=password dbname=roadmap sslmode=disable"
+	db,err:=sql.Open("postgres",conectStr)
+
+	if err!=nil{
+		panic(err)
+	}
+
+
+	rows,err:=db.Query("select * from users")
+
+	if err != nil {
+		panic(err)
+	}
+	users := []user{}
+	for rows.Next(){
+		p := user{}
+		err := rows.Scan(&p.id, &p.name, &p.surname)
+		if err != nil{
+			fmt.Println(err)
+			continue
+		}
+		users = append(users, p)
+	}
+
+	for _,u:=range users{
+		fmt.Println(u.id,u.name,u.surname)
+	}
+
+	reader := bufio.NewReader(os.Stdin)
+	for  {
+		fmt.Println("->")
+		text,_:=reader.ReadString('\n')
+
+		fmt.Println(text)
+	}
+
 
 }
